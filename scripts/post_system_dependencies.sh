b984055d389663d16fbda3aab5b6cebcb002b2e4
shaloo <5890484+shaloo@users.noreply.github.com>
2023-07-30 20:59:28 +0530

    fix build error 1

--- scripts/post_system_dependencies.sh
+++ scripts/post_system_dependencies.sh
@@ -1,6 +1,6 @@
 export SETUPTOOLS_USE_DISTUTILS=stdlib
 echo $SETUPTOOLS_USE_DISTUTILS=stdlib
 echo "Now installing libenchant"
-sudo apt-get update -y && sudo apt-get install -y build-essential make libenchant-dev enchant git dialog apt-utils && sudo apt-get autoremove && sudo apt-get clean
+apt-get install -y build-essential make libenchant-dev enchant git dialog apt-utils && sudo apt-get autoremove && sudo apt-get clean
 echo "Done"
 python3 -m pip install --upgrade setuptools
